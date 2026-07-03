54f2bc6962dbc6232137621cccbb906ba87daac2
PixelHorse <admin@pixelhorse.in>
2026-07-03 13:13:45 +0530

    Fix file name case

--- src/components/Verticalsection.jsx
+++ src/components/Verticalsection.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState, useCallback } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import "./VerticalSection.css";
+import "./Verticalsection.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
